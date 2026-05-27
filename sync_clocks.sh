0f9049dbd399ae07df631cc0636103375bea138d
habiwan <48130922+habiwan@users.noreply.github.com>
2026-05-27 16:21:25 +0100

    Update sync_clocks.sh

--- sync_clocks.sh
+++ sync_clocks.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+# don't forget to adjust the paths!
+# in this example, I mounted the NAS and the UbuntuVM that runs the docker compose LAMP system 
+# The UbuntuVM that runs the docker LAMP has it's volume mapped from /home/habiwan/docker/docker-compose-lamp/www to /var/www/html 
+# e.g. - ${DOCUMENT_ROOT-./www}:/var/www/html:rw in order to be persistent and simple to maintain
+
 # Paths to the files we are watching
-TIMES_FILE="/home/javier/nfc/times.csv"
-NAMES_FILE="/home/javier/nfc/names.csv"
+TIMES_FILE="/home/habiwan/nfc/times.csv"
+NAMES_FILE="/home/habiwan/nfc/names.csv"
 
 echo "Watching $TIMES_FILE and $NAMES_FILE for changes..."
 
@@ -11,8 +16,8 @@ while inotifywait -e modify "$TIMES_FILE" "$NAMES_FILE"; do
     echo "Change detected! Copying files..."
     
     # copy commands
-    cp /home/javier/nfc/*.csv /media/NAS/PiClock/nfc/
-    cp /home/javier/nfc/*.csv /media/uvm/
+    cp /home/habiwan/nfc/*.csv /media/NAS/PiClock/nfc/
+    cp /home/habiwan/nfc/*.csv /media/uvm/
     
     echo "Sync complete."
 done
